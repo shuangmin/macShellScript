f8f722e5d942899437349c61131eceea9b4122ae
RanderC <chenshuangmin@bigo.sg>
2020-04-01 12:34:47 +0800

    init
    Signed-off-by: RanderC <chenshuangmin@bigo.sg>

---
+++ readme.txt
@@ -0,0 +1,2 @@
+1.字符串通过分隔符将列转为行
+./stringRowToColumn.sh "," a.txt
---
+++ stringRowToColumn.sh
@@ -0,0 +1,2 @@
+echo $1 $2
+awk -F $1 '{for(i=1;i<=NF;i++) a[i,NR]=$i}END{for(i=1;i<=NF;i++) {for(j=1;j<=NR;j++) printf a[i,j] " ";print ""}}' $2
